817ccbdc4cc7a25684534e6a367e19784d14c60a
aliligam <2271982545@qq.com>
2023-06-14 15:46:56 +0800

    arr scanf

--- test/main.cpp
+++ test/main.cpp
@@ -2,23 +2,41 @@
 
 #include<stdio.h>
 #include<string.h>
-
-struct Book
-{
-	char name[20];
-	int price;
-};
+#include<windows.h>//Sleep();  ms
+#include<stdlib.h>//system("")
 
 int main()
 {
-	struct Book b1 = { "c programming",60 };
-	struct Book* p = &b1;
-	printf("%s\n", b1.name);
-	printf("%d\n", (*p).price);
-	strcpy((*p).name, "你好书");
-	(*p).price = 15;
-	printf("%s\n", p->name);
+	
+	char arr[30],arr1[30];
+	int i = 0,all=0;
+	scanf("%[^\n]", arr);
+	printf("output:\n");
+	for (i = 0; arr[i] != '\0'; i++)
+		all++;
+	//all = strlen(arr);
+	for (i = 0; i < all; i++)
+		arr1[i] = '#';
+	for (i = 0; i < all / 2+all%2 ; i++)
+	{
+		arr1[i] = arr[i];
+		arr1[all-1 - i] = arr[all-1 - i];
+		system("cls");
+		printf("%.*s\n", all,arr1);
+		Sleep(200);
+	}
 
-	printf("%d\n", p->price);
+	//char arr[] = "welcome to henan!";
+	//char arr1[] = "#################";
+	//int n = sizeof(arr) - 2;//[]
+	//for (int i = 0; i < sizeof(arr) / 2; i++)
+	//{
+	//	arr1[i] = arr[i];
+	//	arr1[n - i] = arr[n - i];
+	//	printf("%s\n", arr1);
+	//}
+
+
+
 	return 0;
 }
